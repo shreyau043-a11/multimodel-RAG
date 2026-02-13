a8ac6baf231021e2579566dccfc612d8945d6443
shreyau043-a11 <shreyau043@gmail.com>
2026-02-13 12:19:27 +0530

    Update chunking.py

--- rag/chunking.py
+++ rag/chunking.py
@@ -1,10 +1,5 @@
-def chunk_text(text, chunk_size=500, overlap=100):
+def chunk_text(text, chunk_size=500):
     chunks = []
-    start = 0
-
-    while start < len(text):
-        end = start + chunk_size
-        chunks.append(text[start:end])
-        start += chunk_size - overlap
-
+    for i in range(0, len(text), chunk_size):
+        chunks.append(text[i:i + chunk_size])
     return chunks
